b6a1c0ba9924f4edf497c14ef7ba5923efa1f4b3
Henrik Friedrichsen <hrkfrd@gmail.com>
2008-08-25 21:51:33 +0200

    enter your login data here

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+/* MPD data */
+#define HOSTNAME	"localhost"
+#define PORT		6600
+#define PASSWORD	""
+
+/* Last.fm data */
+#define LUSER		""
+#define	LPASSWORD	""
+
+#endif
