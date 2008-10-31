3ce63c6b0c4d09c9e9264d1c97bdc12322d86e8b
hrkfrd <hrkfrd@gmail.com>
2008-10-31 23:27:12 +0100

    add getters and setters

--- config.h
+++ config.h
@@ -5,8 +5,17 @@ class CConfig
 {
 	public:
 		CConfig();
+
+		std::string getLUsername() { return _lusername; }
+		std::string getLPassword() { return _lpassword; }
+		std::string getMHost() { return _mhost; }
+		std::string getMPassword() { return _mpassword; }
+		int getMPort() { return _mport; }
 	private:
 		void ParseLine(std::string line);
+		std::string _lusername, _lpassword;
+		std::string _mhost, _mpassword;
+		int _mport;
 };
 
 extern CConfig* Config;
