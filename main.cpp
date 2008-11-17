76816b506bae83442e8c92678281ac792dcef1f9
hrkfrd <hrkfrd@gmail.com>
2008-11-17 23:53:46 +0100

    allow switching to other usernames when root

--- main.cpp
+++ main.cpp
@@ -11,10 +11,37 @@ onclose()
 	if(Config) delete Config;
 }
 
+void
+setid(char* username)
+{
+	passwd* userinfo = 0;
+
+	if(getuid != 0) {
+		eprintf("%s", "You are not root.");
+		exit(EXIT_FAILURE);
+	}
+
+	userinfo = getpwnam(username);
+	if(!userinfo) {
+		eprintf("%s", "The user you specified does not exist.");
+		exit(EXIT_FAILURE);
+	}
+
+	if(setuid(userinfo->pw_uid == -1 || setgid(userinfo->pw_gid))) {
+			eprintf("%s %s", "Could not switch to user", username);
+			exit(EXIT_FAILURE);
+	}
+
+	setenv("HOME", userinfo->pw_dir, 0);
+}
+
 int
 main(int argc, char* argv[])
 {
 	atexit(onclose);
+
+	if(argc >= 2)
+		setid(argv[2]);
 
 	Config = new CConfig();
 
