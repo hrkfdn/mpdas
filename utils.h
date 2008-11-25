11fb3fabd103addea106f2952094e05d52c0eb0d
hrkfrd <hrkfrd@gmail.com>
2008-11-25 21:23:50 +0100

    export fileexists()

--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
+bool fileexists(const char* file);
 void iprintf(const char* fmt, ...);
 void eprintf(const char* fmt, ...);
 std::string md5sum(const char* fmt, ...);
