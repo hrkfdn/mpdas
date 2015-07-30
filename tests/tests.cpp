de44c5fa1bd3d1b4af1e7dd367bda163239be08e
Christopher Fu <chrisf1337@gmail.com>
2015-07-30 17:02:03 -0400

    Some more test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,9 +4,58 @@
 
 TEST_CASE("Song titles are correctly parsed", "[parseSongTitle]")
 {
-    const Song song("Test Artist", "Test Title", "Test Album", 100);
     std::string songArtist, songTitle;
-    parseSongTitle(song, songArtist, songTitle);
-    REQUIRE(songArtist == "Test Artist");
-    REQUIRE(songTitle == "Test Title");
+    SECTION("Regular song")
+    {
+        const Song song1("Test Artist", "Test Title", "Test Album", 100);
+        parseSongTitle(song1, songArtist, songTitle);
+        REQUIRE(songArtist == "Test Artist");
+        REQUIRE(songTitle == "Test Title");
+
+        const Song song2("Test-Artist", "Test Title", "Test Album", 100);
+        parseSongTitle(song2, songArtist, songTitle);
+        REQUIRE(songArtist == "Test-Artist");
+        REQUIRE(songTitle == "Test Title");
+    }
+
+    SECTION("Song with no artist and well-formed title")
+    {
+        const Song song1("", "Test Artist - Test Title", "", 100);
+        parseSongTitle(song1, songArtist, songTitle);
+        REQUIRE(songArtist == "Test Artist");
+        REQUIRE(songTitle == "Test Title");
+
+        const Song song2("", "Test-Artist - Test Title", "", 100);
+        parseSongTitle(song2, songArtist, songTitle);
+        REQUIRE(songArtist == "Test-Artist");
+        REQUIRE(songTitle == "Test Title");
+
+        const Song song3("", "Test-Artist - Test - Title", "", 100);
+        parseSongTitle(song3, songArtist, songTitle);
+        REQUIRE(songArtist == "Test-Artist");
+        REQUIRE(songTitle == "Test - Title");
+
+        const Song song4("", "Test-Artist - Test-Title", "", 100);
+        parseSongTitle(song4, songArtist, songTitle);
+        REQUIRE(songArtist == "Test-Artist");
+        REQUIRE(songTitle == "Test-Title");
+
+        const Song song5("", " - Test Title", "", 100);
+        parseSongTitle(song5, songArtist, songTitle);
+        REQUIRE(songArtist == "");
+        REQUIRE(songTitle == "Test Title");
+    }
+
+    SECTION("Song with no artist and malformed title")
+    {
+        const Song song1("", "Test Title", "", 100);
+        parseSongTitle(song1, songArtist, songTitle);
+        REQUIRE(songArtist == "");
+        REQUIRE(songTitle == "Test Title");
+
+        const Song song2("", "- Test Title", "", 100);
+        parseSongTitle(song2, songArtist, songTitle);
+        REQUIRE(songArtist == "");
+        REQUIRE(songTitle == "- Test Title");
+    }
 }
